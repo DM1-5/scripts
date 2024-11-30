74394259331f33837a06abedcb721f84c669b917
Santiago Sepulveda <santiago.sepulveda1@utp.edu.co>
2024-11-30 10:19:47 -0500

    .

--- patch/Lpatch.sh
+++ patch/Lpatch.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-echo "*Cliente:* $CLIENT *Host:* $(hostname) *Reporte:* Parches de seguridad linux IP: $(hostname -I)" > Lpatch.log
+echo "Cliente: $CLIENT Host: $(hostname) Reporte: Parches de seguridad linux IP: $(hostname -I)" > Lpatch.log
 date >> Lpatch.log
 yum updateinfo list security all > securityPatches.log
 
 spreport() {
   grep "$1" securityPatches.log > "$1"SecurityPatches.log
   num=$(grep -c '^' "$1"SecurityPatches.log)
-  echo "Linux Patch $1: $num" >> Lpatch.log
+  echo "- Linux Patch $1: $num" >> Lpatch.log
 }
 
 spreport Critical
@@ -16,3 +15,5 @@ spreport Important
 
 # Envia el reporte
 mail -s "$(head -n 1 Lpatch.log)" -a "CriticalSecurityPatches.log" -a "ImportantSecurityPatches.log" "$CORREO" < Lpatch.log
+
+rm -f securityPatches.log CriticalSecurityPatches.log ImportantSecurityPatches.log Lpatch.log
